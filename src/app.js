d194eb2e572f8c8798864ba5d7d8455eb08241e4
rama-278 <ramadevibodapati217@gmail.com>
2026-02-27 20:13:51 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,18 +1,51 @@
 const express = require('express');
 const app = express();
 
-app.use("/api", (req, res) => {
-    res.send("Welcome from the Dashboard");
+
+// app.use("/test", (req, res) => {
+//     res.send("Welcome Home!");
+// });
+
+// app.use("/test2", (req, res) => {
+//     res.send("Hello world!");
+// });
+
+// app.use("/hello", (req, res) => {
+//     res.send("Hello hello hello");
+// });
+
+
+// app.use("/", (req, res) => {
+//     res.send("Welcome from the Dashboard");
+// });
+
+
+// app.use("/user", (req, res) => {
+//    res.send("Hellooowwew!");
+// });
+
+
+//This will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Ramadevi", lastName: "Bodapati" });
 });
 
+//This will only handle Post call to /user
+app.post("/user", (req, res) => {
+    res.send("Data successfully saved to database");
+});
 
+//This will handle delete method api calls to /user
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully");
+});
+
+//This will match all HTTP method API calls to /test
 app.use("/test", (req, res) => {
-    res.send("Hello world!");
+   res.send("Hello world!");
 });
 
-app.use("/hello", (req, res) => {
-    res.send("Hello hello hello");
-});
+
 
 app.listen(7777, () => {
     console.log("The Server  is  succesfully listening on port number 3000");

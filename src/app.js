8d887099dc7701880a64667e8abe96d7ec906952
rama-278 <ramadevibodapati217@gmail.com>
2026-02-27 20:49:02 +0530

    exploring advanced routing patterns

--- src/app.js
+++ src/app.js
@@ -2,44 +2,43 @@ const express = require('express');
 const app = express();
 
 
-// app.use("/test", (req, res) => {
-//     res.send("Welcome Home!");
-// });
-
-// app.use("/test2", (req, res) => {
-//     res.send("Hello world!");
-// });
-
-// app.use("/hello", (req, res) => {
-//     res.send("Hello hello hello");
-// });
-
-
-// app.use("/", (req, res) => {
-//     res.send("Welcome from the Dashboard");
-// });
 
 
 
 
 //This will only handle GET call to /user
-app.get("/user", (req, res) => {
+// app.get("/user", (req, res) => {
+//     res.send({ firstName: "Ramadevi", lastName: "Bodapati" });
+// });
+
+// //This will only handle Post call to /user
+// app.post("/user", (req, res) => {
+//     res.send("Data successfully saved to database");
+// });
+
+// //This will handle delete method api calls to /user
+// app.delete("/user", (req, res) => {
+//     res.send("Deleted successfully");
+// });
+
+// //This will match all HTTP method API calls to /test
+// app.use("/test", (req, res) => {
+//    res.send("Hello world!");
+// });
+
+
+
+//Advanced routing techiniques or patterns
+// app.get(/a/ , (req, res) => {
+//     res.send("This is GET method");
+// });
+app.get(/.*fly$/ , (req, res) => {
     res.send({ firstName: "Ramadevi", lastName: "Bodapati" });
 });
 
-//This will only handle Post call to /user
-app.post("/user", (req, res) => {
-    res.send("Data successfully saved to database");
-});
-
-//This will handle delete method api calls to /user
-app.delete("/user", (req, res) => {
-    res.send("Deleted successfully");
-});
-
-//This will match all HTTP method API calls to /test
-app.use("/test", (req, res) => {
-   res.send("Hello world!");
+app.get("/user/:id/:name", (req, res) => {
+    console.log(req.params);
+    res.send({firstName:"Bindu",lastName:"Bodapati"});
 });
 
 

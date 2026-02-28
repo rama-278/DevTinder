e2a98546e82173121dafd5830ad220f853bb8157
rama-278 <ramadevibodapati217@gmail.com>
2026-02-28 19:56:24 +0530

    exploring middleware

--- src/app.js
+++ src/app.js
@@ -32,16 +32,46 @@ const app = express();
 // app.get(/a/ , (req, res) => {
 //     res.send("This is GET method");
 // });
-app.get(/.*fly$/ , (req, res) => {
-    res.send({ firstName: "Ramadevi", lastName: "Bodapati" });
-});
-
-app.get("/user/:id/:name", (req, res) => {
-    console.log(req.params);
-    res.send({firstName:"Bindu",lastName:"Bodapati"});
-});
 
 
+// app.get(/.*fly$/ , (req, res) => {
+//     res.send({ firstName: "Ramadevi", lastName: "Bodapati" });
+// });
+
+// app.get("/user/:id/:name", (req, res) => {
+//     console.log(req.params);
+//     res.send({firstName:"Bindu",lastName:"Bodapati"});
+// });
+
+
+
+app.get("/user", [
+    (req, res, next) => {
+    console.log("Handling the route user 1");
+    //res.send("Handler Respose 1");
+    next();
+},
+    (req, res, next) => {
+    console.log("Handling the route user 2");
+        //res.send("Handler Respose 2");
+        next();
+    }],
+     (req, res, next) => {
+    console.log("Handling the route user 3");
+         //res.send("Handler Respose 3");
+         next();
+    },
+      (req, res, next) => {
+    console.log("Handling the route user 4");
+          //res.send("Handler Respose 4");
+          next();
+    },
+       (req, res, next) => {
+           console.log("Handling the route user 5");
+           res.send("Handler Respose 5");
+},
+
+);
 
 app.listen(7777, () => {
     console.log("The Server  is  succesfully listening on port number 3000");
